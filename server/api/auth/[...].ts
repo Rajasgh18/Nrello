ed0c9c4e0b8bdfc4f41a57dabb722b3f60e71960
Raja Singh <rajasgh18@gmail.com>
2024-05-09 18:51:53 +0530

    auth config

---
+++ server/api/auth/[...].ts
@@ -0,0 +1,55 @@
+import { NuxtAuthHandler } from '#auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { User } from '~/server/models/User.model';
+
+const getUser = async (id: string) => {
+  const user = await User.findById({ id });
+  return user?.toJSON();
+}
+
+export default NuxtAuthHandler({
+  secret: useRuntimeConfig().auth.secret,
+  providers: [
+    // @ts-ignore
+    CredentialsProvider.default({
+      name: "credentials",
+      origin: useRuntimeConfig().auth.origin,
+
+      async authorize(credentials: {
+        email: string,
+        password: string
+      }) {
+        const user = await User.findOne({ email: credentials.email });
+        if (!user) return null;
+
+        const isValid = await user.comparePassword(credentials.password);
+        if (!isValid) return null;
+
+        return user.toObject();
+      }
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token = { ...token, ...user };
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      const refreshedUser = await getUser(token._id as string);
+      session.user = {
+        ...token,
+        ...session.user,
+        ...refreshedUser
+      }
+
+      return session;
+    }
+  }
+});
