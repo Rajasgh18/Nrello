84168777e7c756c63e133cc063419f5b73d952e5
Raja Singh <rajasgh18@gmail.com>
2024-05-12 20:07:30 +0530

    Board: get route

---
+++ server/api/boards/[boardId]/index.get.ts
@@ -0,0 +1,16 @@
+export default defineEventHandler(async (event) => {
+
+  const user = event.context.user;
+  const boardId = getRouterParam(event, 'boardId');
+
+  const board = await Board.findOne({
+    _id: boardId,
+    owner: user._id
+  }).populate({
+    path: 'lists',
+    model: List
+  });
+
+  return board;
+
+});
