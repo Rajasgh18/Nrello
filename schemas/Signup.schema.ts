05a2e443c9250d153a83a1272411a7d8dd6b3513
Raja Singh <rajasgh18@gmail.com>
2024-05-12 20:08:01 +0530

    Schema: sign up

--- schemas/Signup.schema.ts
+++ schemas/Signup.schema.ts
@@ -1,9 +1,8 @@
 import { z } from 'zod';
+import SigninSchema from './Signin.schema';
 
-export default z.object({
+export default SigninSchema.extend({
     name: z.string().min(3).max(50),
-    email: z.string().email(),
-    password: z.string().min(8),
     confirmPassword: z.string().min(8)
 }).refine(data => data.password === data.confirmPassword, {
     message: "Password do not match",
