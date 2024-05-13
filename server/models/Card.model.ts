0c2df88fe2ff0a2ddb96a46d702e86eb13332229
Raja Singh <rajasgh18@gmail.com>
2024-05-13 10:40:22 +0530

    Model: Card

---
+++ server/models/Card.model.ts
@@ -0,0 +1,30 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface CardDocument extends Document {
+    title: string;
+    description: string;
+    owner: string;
+    list: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+const cardSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    list: {
+        type: Schema.Types.ObjectId,
+        ref: 'List',
+        required: true
+    }
+}, { timestamps: true });
+
+export const Card = model<CardDocument>('Card', cardSchema);
