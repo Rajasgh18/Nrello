dce3ccae462e5a63531a42d2fb0490d5302fc76a
Raja Singh <rajasgh18@gmail.com>
2024-05-16 19:35:12 +0530

    Card: post route

---
+++ server/api/lists/[listId]/cards/index.post.ts
@@ -0,0 +1,29 @@
+import { Validator } from '#nuxt-server-utils';
+import CardSchema from '~/schemas/Card.schema';
+
+export default defineEventHandler(async (event) => {
+
+  const body = readBody(event);
+  const user = event.context.user;
+  const listId = getRouterParam(event, 'listId');
+
+  Validator.validateSchema(CardSchema, body);
+
+  const card = await Card.create({
+    ...body,
+    list: listId,
+    owner: user._id
+  });
+
+  await List.findOneAndUpdate({
+    _id: listId,
+    owner: user._id
+  }, {
+    $push: {
+      cards: card._id
+    }
+  });
+
+  return card;
+
+});
