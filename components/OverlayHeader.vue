e530b4f97bcfada3f36dda27e8c7c49160861b32
Raja Singh <rajasgh18@gmail.com>
2024-05-19 21:43:55 +0530

    Component: header

---
+++ components/OverlayHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+interface Props {
+    title: string;
+    onClick: () => void;
+}
+
+defineProps<Props>();
+
+</script>
+
+<template>
+    <div class="py-2 px-4 flex justify-between items-center border-b dark:border-gray-800">
+        <h3 class="font-semibold">{{ title }}</h3>
+        <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" @click="onClick" />
+    </div>
+</template>
+
+<style scoped></style>
