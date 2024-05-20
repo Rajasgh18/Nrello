673ebd3ab4efdbaa48cc79658307030ffdaf51ca
Raja Singh <rajasgh18@gmail.com>
2024-05-20 20:07:16 +0530

    Component: List Card

---
+++ components/List/Card.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { CardDocument } from "~/server/models/Card.model";
+
+interface Props {
+    card: CardDocument;
+};
+
+defineProps<Props>();
+
+</script>
+
+<template>
+    <div class="p-2 border shadow rounded-lg dark:border-gray-600 bg-white dark:bg-gray-700 focus:cursor-grab">
+        {{ card.title }}
+    </div>
+</template>
+
+<style scoped></style>
